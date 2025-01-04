30ce07804860c7302d80d267b11be7c14765f189
kimsokeen <keenmeka@gmail.com>
2025-01-04 23:31:53 +0700

    Update test.py

--- test.py
+++ test.py
@@ -40,26 +40,26 @@ def page1():
         navigate(2)
 
 def page2():
-    st.title("page2")
     back_button(1)
+    st.title("page2")
     if st.button("Get Started"):
         navigate(3)
 
 def page3():
-    st.title("page3")
     back_button(1)
+    st.title("page3")
     if st.button("Get Started"):
         navigate(4)
 
 def page4():
-    st.title("page4")
     back_button(1)
+    st.title("page4")
     if st.button("Get Started"):
         navigate(5)
 
 def page5():
-    st.title("page5")
     back_button(1)
+    st.title("page5")
     if st.button("Get Started"):
         navigate(1)
 
