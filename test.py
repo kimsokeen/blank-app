e4dd483650d8deec7e743afd85d61a22738417fb
kimsokeen <keenmeka@gmail.com>
2025-01-04 22:50:38 +0700

    Update test.py

--- test.py
+++ test.py
@@ -2,4 +2,10 @@ import streamlit as st
 import pandas as pd 
 import numpy as np 
 
+# Initialize page state if not set
+if "page" not in st.session_state:
+    st.session_state.page = 1  # Start from page 1
 
+# Function to navigate to the specified page with single click
+def navigate(page):
+    st.session_state.page = page
